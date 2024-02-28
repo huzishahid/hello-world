c4297c13db805e168fdc8cbedfe25d5ccd265720
huzishahid <161612462+huzishahid@users.noreply.github.com>
2024-02-28 19:39:45 +0500

    my 1st work

---
+++ main.ts
@@ -0,0 +1,41 @@
+// Arithmetic operators
+
+// // console.log (10+20);
+// let num1 = 20;
+// let num2 = 10;
+// console.log (num1 + 
+
+// let num1 = 20;
+// let num2 = 30;
+// console.log (num1 + num2)
+
+// let n =5;
+// n = n + 2;
+// console.log(n);
+
+// let fristname =  "subhan";
+// let lastname = "huzi";
+// console.log(fristname+"  "+lastname);
+
+// comparison operator
+
+// let n = 5;
+// let a = "5"
+// console.log(n == a)
+
+
+// let num = 10;
+// console.log (num >=5);
+
+let num = 10;
+//console.log(num>=5);
+
+// console.log(num <= 10);
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log("num1 + num2");
+
+let n = 5;
+n = n + 1;
+console.log(n);
